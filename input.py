d765bfbb8eb7c82f51f09d1be65f17dd45904ccf
gandhikush25 <kush25610@yahoo.in>
2018-08-17 11:37:04 +0530

    Update input.py

--- input.py
+++ input.py
@@ -1,5 +1,7 @@
 name = input ("Enter Your Name:")
 age = input ("Enter Your Age:")
+contact = input("Your Contact No:")
 print("Your Name is:" + name)
 print("Your Age is:" + age)
+print("Your Contact No is" + contact)
 
